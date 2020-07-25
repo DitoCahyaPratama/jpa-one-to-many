3c4a3db9cd9c621d56910d1acb020ab380c569a1
Dito Cahya Pratama <ditocahyapratama717@gmail.com>
2020-07-25 23:19:43 +0700

    Create Comment and Post Controller

---
+++ src/main/java/com/example/jpa/jpaonetomany/controller/PostController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.jpa.jpaonetomany.controller;
+
+import com.example.jpa.exception.ResourceNotFoundException;
+import com.example.jpa.jpaonetomany.model.Post;
+import com.example.jpa.jpaonetomany.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ *
+ * @author Doyatama
+ */
+@RestController
+public class PostController {
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping("/posts")
+    public Page<Post> getAllPosts(Pageable pageable) {
+        return postRepository.findAll(pageable);
+    }
+
+    @PostMapping("/posts")
+    public Post createPost(@Valid @RequestBody Post post) {
+        return postRepository.save(post);
+    }
+
+    @PutMapping("/posts/{postId}")
+    public Post updatePost(@PathVariable Long postId, @Valid @RequestBody Post postRequest) {
+        return postRepository.findById(postId).map(post -> {
+            post.setTitle(postRequest.getTitle());
+            post.setDescription(postRequest.getDescription());
+            post.setContent(postRequest.getContent());
+            return postRepository.save(post);
+        }).orElseThrow(() -> new ResourceNotFoundException("PostId " + postId + " not found"));
+    }
+
+
+    @DeleteMapping("/posts/{postId}")
+    public ResponseEntity<?> deletePost(@PathVariable Long postId) {
+        return postRepository.findById(postId).map(post -> {
+            postRepository.delete(post);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("PostId " + postId + " not found"));
+    }
+}
